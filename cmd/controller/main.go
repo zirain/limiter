d5de58c9a68e34ce61f626f5e1e7c87028144b54
hejianpeng <hejianpeng2@huawei.com>
2022-07-15 15:21:43 +0800

    add github workflow

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -20,22 +20,18 @@ import (
 	"flag"
 	"os"
 
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-
 	istioclient "istio.io/client-go/pkg/clientset/versioned"
 	istioinformers "istio.io/client-go/pkg/informers/externalversions"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	policyv1alpha1 "github.com/zirain/limiter/api/policy/v1alpha1"
 	"github.com/zirain/limiter/controllers"
-	//+kubebuilder:scaffold:imports
 )
 
 var (
---
+++ hack/golangci-lint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+GOLANGCI_LINT_PKG="github.com/golangci/golangci-lint/cmd/golangci-lint"
+GOLANGCI_LINT_VER="v1.42.1"
+
+cd "${REPO_ROOT}"
+source "hack/util.sh"
+
+if ! [ -x "$(command -v golangci-lint)" ]; then
+    util::install_tools ${GOLANGCI_LINT_PKG} ${GOLANGCI_LINT_VER}
+fi
+
+if golangci-lint run -c $REPO_ROOT/hack/config/.golangci.yml; then
+  echo 'Congratulations!  All Go source files have passed staticcheck.'
+else
+  echo # print one empty line, separate from warning messages.
+  echo 'Please review the above warnings.'
+  echo 'If the above warnings do not make sense, feel free to file an issue.'
+  exit 1
+fi
